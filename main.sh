0103353193e48d9f9ab819d763cd360dba55ae50
QIU Jian <qiujian@meituan.com>
2013-04-27 10:41:52 +0800

    Add python_qa.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,5 @@
-source $GITAWAREPROMPT/colors.sh
-source $GITAWAREPROMPT/prompt.sh
+source $PYTHONDEVENV/colors.sh
+source $PYTHONDEVENV/prompt.sh
+source $PYTHONDEVENV/python_qa.sh
+
+export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
