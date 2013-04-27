dd1ebb4f435c220987b370b07cc27806f91a0a9c
QIU Jian <qiujian@meituan.com>
2013-04-27 11:42:18 +0800

    Update prompt

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@ source $PYTHONDEVENV/colors.sh
 source $PYTHONDEVENV/prompt.sh
 source $PYTHONDEVENV/python_qa.sh
 
-export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+export PS1="\u@\h:\w\[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
